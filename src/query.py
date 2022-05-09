b20200ce4211ce1ab22f8e843008c4d71a79db79
JavaharlalN <grechanichenkos@yandex.ru>
2022-05-09 19:30:54 +0300

    added format

--- src/query.py
+++ src/query.py
@@ -30,5 +30,5 @@ def  match_set(chat_id, query):
 		set.start_game(chat_id)
 	elif query == "сколько":
 		set.persentage(chat_id)
-	# elif query.split(' ')[0] == "формат":
-	# 	set.format(chat_id, ' '.join(query.split))
+	elif query.split(' ')[0] == "формат":
+		set.format(chat_id, ' '.join(query.split(' ')[1:]))
--- src/set.py
+++ src/set.py
@@ -1,28 +1,31 @@
 from datetime import datetime
-from functions import sender, vk_session, get_format
+import functions
 from game import game
 
 
 def start_game(chat_id):
 	game.start()
-	sender(chat_id, "ладно")
+	functions.sender(chat_id, "ладно")
 
 
 def stop_game(chat_id):
 	game.stop()
-	sender(chat_id, "ладно")
+	functions.sender(chat_id, "ладно")
 
 def persentage(chat_id):
 	hm = (datetime.now() - datetime(2021, 9, 1)).total_seconds() / 31536000
 	if hm >= 1:
-		sender(chat_id, "Ну вот и всё, прощай лето... 100%")
+		functions.sender(chat_id, "Ну вот и всё, прощай лето... 100%")
 	else:
-		sender(chat_id, str(hm * 100) + '%')
-	fmt = get_format(chat_id)
+		functions.sender(chat_id, str(hm * 100) + '%')
+	fmt = functions.get_format(chat_id)
 	if fmt:
-		vk_session.method("messages.editChat", {
+		functions.vk_session.method("messages.editChat", {
 			"chat_id": chat_id,
 			"title": format(fmt, str(hm)[2:])
 		})
 	else:
-		sender(chat_id, "формат не установлен")
+		functions.sender(chat_id, "формат не установлен")
+
+def format(chat_id, format):
+	functions.set_format(chat_id, format)
