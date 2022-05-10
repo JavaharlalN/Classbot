80fe9a863e09ff97a790f67d727e6329aaec930d
JavaharlalN <grechanichenkos@yandex.ru>
2022-05-10 13:21:35 +0300

    Add `remindeders_sender`

--- src/main.py
+++ src/main.py
@@ -1,20 +1,27 @@
+from concurrent.futures import thread
 import vk_api, datetime
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import query
-from functions import longpoll
+from functions import longpoll, reminders_sender
 from data import Timetable
 from reminders import Reminders
+import threading
+
+timetable = Timetable()
+reminders = Reminders()
+
+threading.Thread(target=reminders_sender, args=(reminders))
 
 while True:
 	now = datetime.datetime.now()
 	print(now)
 	print(str((datetime.datetime.now() - datetime.datetime(2021, 9, 1)).total_seconds() / 315360) + '%')
-	timetable = Timetable()
-	reminders = Reminders()
 	try:
 		for event in longpoll.check():
 			try:
 				if event.type == VkBotEventType.MESSAGE_NEW:
+					with open("id", 'w') as f:
+						f.write(str(event.chat_id))
 					print(event.chat_id)
 					msg = event.message["text"].lower()
 					query.match(event.chat_id, msg, timetable, reminders. is_game)
